0eae7825b629dcadb4b3c918c68ae04b11a92fab
vichet vatt <vichetvattedu@gmail.com>
2022-12-07 00:39:45 +0700

    Profile

--- lib/pages/main/controllers/bottom_navigator_controller.dart
+++ lib/pages/main/controllers/bottom_navigator_controller.dart
@@ -2,12 +2,14 @@ import 'package:ecommerce/pages/home/ui/index.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
+import '../ui/account_ui.dart';
+
 class BottomNavigatorController extends GetxController {
   RxInt index = 0.obs;
   var views = [
     HomePage(),
     Container(),
-    Container(),
+    const AccountView(),
   ];
   void changeIndex(int i) => index.value = i;
 }
---
+++ lib/pages/main/ui/account_ui.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AccountView extends StatelessWidget {
+  const AccountView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          const Expanded(
+            child: const Align(
+              alignment: Alignment.center,
+              child: CircleAvatar(
+                radius: 75,
+                backgroundColor: Colors.black,
+                backgroundImage: AssetImage('assets/images/6.jpg'),
+              ),
+            ),
+          ),
+          const Text(
+            "Vatt Vichet",
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+              flex: 2,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color.fromARGB(100, 120, 116, 116),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const Text(
+                      "About",
+                      textAlign: TextAlign.center,
+                    ),
+                    const Text(""),
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
